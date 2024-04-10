d8e2ce144dc821102823e8aa27389c243522cbc3
Shivani <shivanii@umich.edu>
2024-04-10 13:11:54 -0400

    Modified clear.py and README.md

--- cupid/clear.py
+++ cupid/clear.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-import yaml
+import cupid.util
 
 def clearFolder(folderPath):
     #Clears all contents in the specified folder at folderPath (i.e. computed_notebooks)
@@ -19,20 +19,21 @@ def clearFolder(folderPath):
         print(f"All contents in {folderPath} have been cleared.")
     except Exception as e:
         print(f"Error occurred while clearing contents of the file at path {folderPath}: {e}")
-        
+
 def readConfigFile(configFilePath):
     #Given the file path to config.yml, this function reads the config file content and 
     #returns the val of the run_dir string with '/computed_notebooks' appended to it 
     try:
-        with open(configFilePath, 'r') as file:
-            configContent = yaml.safe_load(file)
-            run_dir = configContent.get('data_sources', {}).get('run_dir')
-            if run_dir:
-                #Append '/computed_notebooks' to the run_dir value if it is not empty
-                fullPath = os.path.join(run_dir, 'computed_notebooks')
-                return fullPath
-            else: #run_dir is empty/wasn't found in config file so return error
-                raise ValueError("'run_dir' was not found in the config file.")
+        #Obtain the contents of the config.yml file and extract the run_dir variable
+        control = cupid.util.get_control_dict(configFilePath)
+        run_dir = control['data_sources']['run_dir']
+        
+        if run_dir:
+            #Append '/computed_notebooks' to the run_dir value if it is not empty
+            fullPath = os.path.join(run_dir, 'computed_notebooks')
+            return fullPath
+        else: #run_dir is empty/wasn't found in config file so return error
+            raise ValueError("'run_dir' was not found in the config file.")
     except FileNotFoundError:
         print(f"config.yml at path'{configFilePath}' not found.")
     except Exception as e:
@@ -45,8 +46,8 @@ def clear():
     #obtain the path to the config.yml file in the current working directory
     currWorkingDir =  os.getcwd() 
     configFilePath = os.path.join(currWorkingDir, 'config.yml')
-   
+
     run_dir = readConfigFile(configFilePath)
-    
+
     if run_dir:
         clearFolder(run_dir)
