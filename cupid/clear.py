d7b2f6a6f97b10a4b76fc75ec2c111760a2f4539
Shivani <shivanii@umich.edu>
2024-04-11 12:19:21 -0400

    Updated clear.py and README.md

--- cupid/clear.py
+++ cupid/clear.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import click
 import cupid.util
 
 def clearFolder(folderPath):
@@ -18,36 +19,27 @@ def clearFolder(folderPath):
         os.rmdir(folderPath)    
         print(f"All contents in {folderPath} have been cleared.")
     except Exception as e:
-        print(f"Error occurred while clearing contents of the file at path {folderPath}: {e}")
+        print(f"Error occurred while clearing contents in {folderPath}: {e}")
 
-def readConfigFile(configFilePath):
+def readConfigFile(config_path):
     #Given the file path to config.yml, this function reads the config file content and 
     #returns the val of the run_dir string with '/computed_notebooks' appended to it 
-    try:
-        #Obtain the contents of the config.yml file and extract the run_dir variable
-        control = cupid.util.get_control_dict(configFilePath)
-        run_dir = control['data_sources']['run_dir']
-        
-        if run_dir:
-            #Append '/computed_notebooks' to the run_dir value if it is not empty
-            fullPath = os.path.join(run_dir, 'computed_notebooks')
-            return fullPath
-        else: #run_dir is empty/wasn't found in config file so return error
-            raise ValueError("'run_dir' was not found in the config file.")
-    except FileNotFoundError:
-        print(f"config.yml at path'{configFilePath}' not found.")
-    except Exception as e:
-        print(f"Error occurred while reading config file at path '{configFilePath}': {e}")
-    return None
-
-#Entry point to this script
-def clear():
-    #Get the current working directory and add 'config.yml' to the path to 
-    #obtain the path to the config.yml file in the current working directory
-    currWorkingDir =  os.getcwd() 
-    configFilePath = os.path.join(currWorkingDir, 'config.yml')
-
-    run_dir = readConfigFile(configFilePath)
-
+    
+    #Obtain the contents of the config.yml file and extract the run_dir variable
+    control = cupid.util.get_control_dict(config_path)
+    run_dir = control['data_sources'].get('run_dir', None)
+    
     if run_dir:
-        clearFolder(run_dir)
+        #Append '/computed_notebooks' to the run_dir value if it is not empty
+        fullPath = os.path.join(run_dir, 'computed_notebooks')
+        return fullPath
+    
+    else: #run_dir is empty/wasn't found in config file so return error
+        raise ValueError("'run_dir' was empty/not found in the config file.")
+
+@click.command()
+@click.argument('config_path')
+#Entry point to this script
+def clear(config_path):
+    run_dir = readConfigFile(config_path)
+    clearFolder(run_dir)
