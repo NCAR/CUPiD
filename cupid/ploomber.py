39dc30cb79cfe2b3449f4fb57a770aa377edeb5b
Teagan King <98482480+TeaganKing@users.noreply.github.com>
2024-05-15 15:27:28 -0600

    Create ploomber.py

---
+++ cupid/ploomber.py
@@ -0,0 +1,147 @@
+class ScriptRunner(NotebookMixin, Task):
+    """
+    Similar to NotebookRunner, except it uses python to run the code,
+    instead of papermill, hence, it doesn't generate an output notebook. But
+    it also works by injecting a cell into the source code. Source can be
+    a ``.py`` script or an ``.ipynb`` notebook. **Does not support magics.**
+
+    Parameters
+    ----------
+    source: str or pathlib.Path
+        Script source, if str, the content is interpreted as the actual
+        script, if pathlib.Path, the content of the file is loaded. When
+        loading from a str, ext_in must be passed
+    product: ploomber.File
+        The output file
+    dag: ploomber.DAG
+        A DAG to add this task to
+    name: str, optional
+        A str to indentify this task. Should not already exist in the dag
+    params: dict, optional
+        Script parameters. This are passed as the "parameters" argument
+        to the papermill.execute_notebook function, by default, "product"
+        and "upstream" are included
+    ext_in: str, optional
+        Source extension. Required if loading from a str. If source is a
+        ``pathlib.Path``, the extension from the file is used.
+    static_analysis : ('disabled', 'regular', 'strict'), default='regular'
+        Check for various errors in the script. In 'regular' mode, it aborts
+        execution if the notebook has syntax issues, or similar problems that
+        would cause the code to break if executed. In 'strict' mode, it
+        performs the same checks but raises an issue before starting execution
+        of any task, furthermore, it verifies that the parameters cell and
+        the params passed to the notebook match, thus, making the script
+        behave like a function with a signature.
+    local_execution : bool, optional
+        Change working directory to be the parent of the script source.
+        Defaults to False.
+
+    Examples
+    --------
+
+    Spec API:
+
+    .. code-block:: yaml
+        :class: text-editor
+        :name: pipeline-yaml
+
+        tasks:
+          - source: script.py
+            class: ScriptRunner
+            product:
+                data: data.csv
+                another: another.csv
+
+    Python API:
+
+    >>> from pathlib import Path
+    >>> from ploomber import DAG
+    >>> from ploomber.tasks import ScriptRunner
+    >>> from ploomber.products import File
+    >>> dag = DAG()
+    >>> product = {'data': File('data.csv'), 'another': File('another.csv')}
+    >>> _ = ScriptRunner(Path('script.py'), product, dag=dag)
+    >>> _ = dag.build()
+    """
+
+    def __init__(
+        self,
+        source,
+        product,
+        dag,
+        name=None,
+        params=None,
+        ext_in=None,
+        static_analysis="regular",
+        local_execution=False,
+    ):
+        self.ext_in = ext_in
+
+        kwargs = dict(hot_reload=dag._params.hot_reload)
+        self._source = ScriptRunner._init_source(
+            source, kwargs, ext_in, static_analysis, False, False
+        )
+        self.local_execution = local_execution
+        super().__init__(product, dag, name, params)
+
+    @staticmethod
+    def _init_source(
+        source,
+        kwargs,
+        ext_in=None,
+        static_analysis="regular",
+        extract_up=False,
+        extract_prod=False,
+    ):
+        ns = NotebookSource(
+            source,
+            ext_in=ext_in,
+            static_analysis=static_analysis,
+            kernelspec_name=None,
+            check_if_kernel_installed=False,
+            **kwargs,
+        )
+        ns._validate_parameters_cell(extract_up, extract_prod)
+        return ns
+
+    def run(self):
+        # regular mode: raise but not check signature
+        # strict mode: called at render time
+        if self.static_analysis == "regular":
+            self.source._check_notebook(raise_=True, check_signature=False)
+
+        fd, tmp = tempfile.mkstemp(".py")
+        os.close(fd)
+
+        code = "\n\n".join(
+            [
+                c["source"]
+                for c in self.source.nb_obj_rendered.cells
+                if c["cell_type"] == "code"
+            ]
+        )
+
+        cwd = str(self.source.loc.parent.resolve())
+        orig_env = os.environ.copy()
+
+        if "PYTHONPATH" not in orig_env:
+            orig_env["PYTHONPATH"] = cwd
+        else:
+            orig_env["PYTHONPATH"] += os.pathsep + cwd
+
+        tmp = Path(tmp)
+        tmp.write_text(code)
+
+        if self.source.language == "python":
+            interpreter = _python_bin()
+        elif self.source.language == "r":
+            interpreter = "Rscript"
+        else:
+            raise ValueError("ScriptRunner only works with Python and R scripts")
+
+        try:
+            _run_script_in_subprocess(interpreter, tmp, cwd, orig_env)
+        except Exception as e:
+            raise TaskBuildError("Error when executing task" f" {self.name!r}.") from e
+        finally:
+            tmp.unlink()
