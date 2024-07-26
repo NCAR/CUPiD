5b6e3d565dc910284b29dbc6577b45f840033df1
Michael Levy <mike.levy.work@gmail.com>
2024-07-26 17:42:22 -0600

    More xarray to replace netcdf4
    Read in 40 years of SMB data with read_cesm_smb(), and then use xarray
    functions (basically taking means or sums over different dimensions) to compute
    the different climatologies. This greatly reduces the number of functions
    needed in utils - it's just read_cesm_smb() and some plotting routines.

--- examples/nblibrary/glc/utils.py
+++ examples/nblibrary/glc/utils.py
@@ -5,59 +5,13 @@ import os
 import numpy as np
 import xarray as xr
 from matplotlib import pyplot as plt
-from netCDF4 import Dataset
 
 
-def set_plot_prop_clean(ax):
+def read_cesm_smb(path, case_name, last_year, params):
     """
-    This function cleans up the figures from unnecessary default figure properties.
-
+    This function reads CESM coupler history files and returns
+    an xarray DataArray containing surface mass balance in units mm/y
     """
-    ax.invert_yaxis()
-    ax.set_xlabel("")
-    ax.set_ylabel("")
-    ax.set_xticklabels("")
-    ax.set_yticklabels("")
-    ax.set_xticks([])
-    ax.set_yticks([])
-
-
-def rmse(prediction, target):
-    """
-    This function returns the root mean square error for the SMB.
-    Input:
-        prediction = field to predict
-        target = field to compare with the prediction
-    """
-    return np.sqrt(((prediction - target) ** 2).mean())
-
-
-def net_avrg(data):
-    """
-    This function returns the net average of a data field
-    """
-    return np.sum(np.sum(data, axis=0), axis=0)
-
-
-def read_smb(file):
-    """
-    This function reads the CISM SMB dataset from a CESM simulation output
-    in the cpl directory. The output is adjusted to be converted to mm/yr w.e unit.
-
-    Input:
-        file: name of the file to extract the SMB
-    """
-    rhoi = 917  # ice density kg/m3
-    sec_in_yr = 60 * 60 * 24 * 365  # seconds in a year
-    smb_convert = sec_in_yr / rhoi * 1000  # converting kg m-2 s-1 ice to mm y-1 w.e.
-
-    nid = Dataset(file, "r")
-    smb_cism = np.squeeze(nid.variables["glc1Exp_Flgl_qice"][0, :, :]) * smb_convert
-    nid.close()
-    return smb_cism
-
-
-def _get_cesm_output(path, case_name, last_year, params):
     # Set parameters
     rhoi = 917  # ice density kg/m3
     sec_in_yr = 60 * 60 * 24 * 365  # seconds in a year
@@ -94,33 +48,28 @@ def _get_cesm_output(path, case_name, last_year, params):
     return climo_out
 
 
-def create_climo(path, case_name, last_year, params):
-
-    climo_out = _get_cesm_output(path, case_name, last_year, params)
-
-    # Averaging the climo data
-    return climo_out.mean("time").data
+# -------------------- #
+#  PLOTTING FUNCTIONS  #
+# -------------------- #
 
 
-def compute_annual_climo(path, case_name, last_year, params):
-    # Initializing a field for the climatology
-    avg_smb_timeseries = np.zeros(last_year)
-    climo_out = _get_cesm_output(path, case_name, last_year, params)
-    for k in range(len(climo_out["time"])):
-        # index into avg_smb_timeseries; want largest k to index (last_year-1)
-        kk = last_year - (len(climo_out["time"]) - k)
-        # note that mm_to_Gt has 1 / res**2 factor, so we want a sum rather than mean
-        avg_smb_timeseries[kk] = np.round(
-            climo_out.isel(time=k).sum() * params["mm_to_Gt"],
-            2,
-        ).data
+def set_plot_prop_clean(ax):
+    """
+    This function cleans up the figures from unnecessary default figure properties.
 
-    return last_year - len(climo_out["time"]) + 1, avg_smb_timeseries
+    """
+    ax.invert_yaxis()
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_xticklabels("")
+    ax.set_yticklabels("")
+    ax.set_xticks([])
+    ax.set_yticks([])
 
 
-def plot_contour(data, fig, ax, title, vmin, vmax, cmap, mm_to_Gt):
-    avg_data = np.round(net_avrg(data) * mm_to_Gt, 2)
-    last_panel0 = ax.imshow(data[:, :], vmin=vmin, vmax=vmax, cmap=cmap)
+def plot_contour(da, fig, ax, title, vmin, vmax, cmap, mm_to_Gt):
+    avg_data = np.round(da.sum().data * mm_to_Gt, 2)
+    last_panel0 = ax.imshow(da.data[:, :], vmin=vmin, vmax=vmax, cmap=cmap)
     ax.set_title(title, fontsize=16)
     set_plot_prop_clean(ax)
     ax.annotate("net avg =" + str(avg_data) + " Gt/yr", xy=(5, 5), fontsize=16)
@@ -129,22 +78,6 @@ def plot_contour(data, fig, ax, title, vmin, vmax, cmap, mm_to_Gt):
     cax = fig.add_axes([0.35, pos.y0, 0.02, pos.y1 - pos.y0])
 
     cbar = fig.colorbar(last_panel0, cax=cax)
-    cbar.ax.tick_params(labelsize=16)
-
-
-def plot_contour_diff(data_new, data_old, fig, ax, title, vmin, vmax, cmap, mm_to_Gt):
-    avg_data = np.round(net_avrg(data_new - data_old) * mm_to_Gt, 2)
-    last_panel2 = ax.imshow(data_new - data_old, vmin=vmin, vmax=vmax, cmap=cmap)
-
-    ax.set_title(title, fontsize=16)
-    set_plot_prop_clean(ax)
-
-    ax.annotate("net avg =" + str(avg_data) + " Gt/yr", xy=(5, 5), fontsize=16)
-
-    pos = ax.get_position()
-    cax = fig.add_axes([0.89, pos.y0, 0.02, pos.y1 - pos.y0])
-
-    cbar = fig.colorbar(last_panel2, cax=cax)
     cbar.ax.tick_params(labelsize=16)
 
 
