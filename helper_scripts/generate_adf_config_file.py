49a0f9933841de4305f8cfb0e961cf56f9ba1cf8
Brian Medeiros <skymath@gmail.com>
2024-10-23 15:05:21 -0600

    add more details for ADF config

--- helper_scripts/generate_adf_config_file.py
+++ helper_scripts/generate_adf_config_file.py
@@ -35,38 +35,54 @@ def generate_adf_config(cupid_file, adf_file, out_file):
     with open(adf_file, encoding="UTF-8") as a:
         a_dict = yaml.load(a, Loader=yaml.SafeLoader)
 
-    # Mapping from CUPID's global_params:
-    a_dict["diag_cam_climo"]["cam_case_name"] = c_dict["global_params"]["case_name"]
-    a_dict["diag_cam_baseline_climo"]["cam_case_name"] = c_dict["global_params"][
-        "base_case_name"
-    ]
+    # read parameters from CUPID
+    # use `get` to default to None
+    DOUT = c_dict["global_params"]["CESM_output_dir"]
+    base_case_name = c_dict["global_params"].get("base_case_name")
+    test_case_name = c_dict["global_params"]["case_name"]
+    c_ts = c_dict["timeseries"]
+    ts_case_names = c_ts.get("case_name")
+    if not ts_case_names:
+        raise ValueError("CUPiD file does not have timeseries case_name array.")
 
-    # QUESTION: how to specify locations for model output?
-    # - separate history files, time series files, climo files ?
-    # - separate for 'base case' and 'test case(s)'?
-    a_dict["diag_cam_climo"]["cam_hist_loc"] = c_dict["global_params"][
-        "CESM_output_dir"
-    ]
-    a_dict["diag_cam_baseline_climo"]["cam_hist_loc"] = c_dict["global_params"][
-        "CESM_output_dir"
-    ]
+    # Set case names for ADF config
+    a_dict["diag_cam_climo"]["cam_case_name"] = test_case_name
+    a_dict["diag_cam_baseline_climo"]["cam_case_name"] = base_case_name
 
-    # QUESTION: how to specify different start/end dates for 'base case' and 'test case(s)'?
-    a_dict["diag_cam_climo"]["start_year"] = int(
-        c_dict["global_params"]["start_date"].split("-")[0]
-    )
-    a_dict["diag_cam_climo"]["end_year"] = int(
-        c_dict["global_params"]["end_date"].split("-")[0]
-    )
-    a_dict["diag_cam_baseline_climo"]["start_year"] = int(
-        c_dict["global_params"]["start_date"].split("-")[0]
-    )
-    a_dict["diag_cam_baseline_climo"]["end_year"] = int(
-        c_dict["global_params"]["end_date"].split("-")[0]
-    )
+    
+    # TEST CASE HISTORY FILE PATH
+    a_dict["diag_cam_climo"]["cam_hist_loc"] = "/".join([DOUT, "atm", "hist"])
+    # TEST CASE TIME SERIES FILE PATH
+    a_dict["diag_cam_climo"]["cam_ts_loc"] = "/".join([DOUT, "proc", "tseries"])
+    # TEST CASE CLIMO FILE PATH
+    a_dict["diag_cam_climo"]["cam_climo_loc"] = "/".join([DOUT, "proc", "climo"])
+    # TEST CASE START / END YEARS
+    test_case_cupid_ts_index =  ts_case_names.index(test_case_name) if test_case_name in ts_case_names else None
+    start_date = get_date_from_ts(c_ts["atm"], "start_years", test_case_cupid_ts_index)
+    end_date = get_date_from_ts(c_ts["atm"], "end_years", test_case_cupid_ts_index)
+    a_dict["diag_cam_climo"]["start_year"] = start_date
+    a_dict["diag_cam_climo"]["end_year"] = end_date
+
+    # Set values for BASELINE 
+    base_case_cupid_ts_index = ts_case_names.index(base_case_name) if base_case_name in ts_case_names else None
+    if base_case_name is not None:
+        base_case_output_dir = c_dict["global_params"].get("base_case_output_dir", DOUT)
+        base_start_date = get_date_from_ts(c_ts["atm"], "start_years", base_case_cupid_ts_index)
+        base_end_date = get_date_from_ts(c_ts["atm"], "end_years", base_case_cupid_ts_index)
+        if base_start_date is None:
+            base_start_date = start_date
+        if base_end_date is None:
+            base_end_date = end_date
+
+    a_dict["diag_cam_baseline_climo"]["cam_hist_loc"] = "/".join([base_case_output_dir, "atm", "hist"])
+    a_dict["diag_cam_baseline_climo"]["cam_ts_loc"] = "/".join([base_case_output_dir, "proc", "tseries"])
+    a_dict["diag_cam_baseline_climo"]["cam_climo_loc"] = "/".join([base_case_output_dir, "proc", "climo"])
+    a_dict["diag_cam_baseline_climo"]["start_year"] = base_start_date
+    a_dict["diag_cam_baseline_climo"]["end_year"] = base_end_date
 
     a_dict["diag_basic_info"]["num_procs"] = c_dict["timeseries"].get("num_procs", 1)
-
+    a_dict["diag_basic_info"]["cam_regrid_loc"] = "/".join([DOUT, "proc", "regrid"])  # This is where ADF will make "regrid" files
+    a_dict["diag_basic_info"]["cam_diag_plot_loc"] = "/".join([c_dict["sname"], "ADF"]) # this is where ADF will put plots, and "website" directory
     a_dict["user"] = os.getenv("USER")
 
     with open(out_file, "w") as f:
@@ -80,6 +96,20 @@ def generate_adf_config(cupid_file, adf_file, out_file):
         f.write(f"# Output: {out_file = }\n")
         # enter in each element of the dictionary into the new file
         yaml.dump(a_dict, f, sort_keys=False)
+
+
+def get_date_from_ts(data: dict, keyname: str, listindex: int, default=None):
+    if type(data) is not dict:
+        raise TypeError(f"first argument needs to be dict, got {type(data)}")
+    if keyname not in data:
+        raise KeyError(f"no entry {keyname} in the dict")
+    x = data[keyname]
+    if isinstance(x, list):
+        return x[listindex]
+    elif isinstance(x, int):
+        return x
+    else:
+        return default
 
 
 if __name__ == "__main__":
