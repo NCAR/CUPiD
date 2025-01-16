dcfc44a43c4de7a99eb7feea478d14295262e91f
Teagan King <tking@ucar.edu>
2025-01-16 09:00:33 -0700

    helper script ilamb working

--- helper_scripts/generate_ilamb_config_files.py
+++ helper_scripts/generate_ilamb_config_files.py
@@ -12,7 +12,7 @@ def _parse_args():
     """Parse command line arguments"""
 
     parser = argparse.ArgumentParser(
-        description="Generate cupid_adf_config.yml based on an existing CUPID YAML file",
+        description="Generate an ILAMB model_setup.txt file based on an existing CUPID YAML file",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     # Command line argument for location of CESM source code (required)
@@ -29,27 +29,25 @@ def _parse_args():
         action="store",
         dest="cupid_config_loc",
         default=None,
-        help="CUPiD example to use as template for config.yml",
+        help="CUPiD config file to use information from for model_setup.txt",
     )
     parser.add_argument(
         "--run-type",
         action="store",
         required=True,
-        help="either BGC (biogeochemistry) or SP (satellite phenology)",
+        help="either 'BGC' (biogeochemistry) or 'SP' (satellite phenology)",
     )
     parser.add_argument(
         "--out-dir",
         action="store",
         required=True,
-        help="the output directory where config files are saved",
+        help="the output directory where model_setup.txt file is saved",
     )
     return parser.parse_args()
 
 
-def generate_ilamb_config(cesm_root, cupid_config_loc, run_type, out_dir):
-    """Use cupid config file (YAML) from cupid_config_loc and adf_file (YAML)
-    to produce out_file by modifying adf_file with data from cupid config file.
-    """
+def generate_ilamb_model_setup(cesm_root, cupid_config_loc, run_type, out_dir):
+    """Create model_setup.txt file for use in ILAMB"""
     sys.path.append(os.path.join(cesm_root, "cime"))
 
     cupid_root = os.path.join(cesm_root, "tools", "CUPiD")
@@ -59,69 +57,39 @@ def generate_ilamb_config(cesm_root, cupid_config_loc, run_type, out_dir):
     if not os.path.exists(os.path.join(cupid_config_loc, "config.yml")):
         raise KeyError(f"Can not find config.yml in {cupid_config_loc}")
 
-    # with open(os.path.join(cupid_config_loc, "config.yml")) as c:
-    #     c_dict = yaml.safe_load(c)
+    with open(os.path.join(cupid_config_loc, "config.yml")) as c:
+        c_dict = yaml.safe_load(c)
+    base_case_output_dir = os.path.join(
+        c_dict["global_params"]["CESM_output_dir"],
+        c_dict["global_params"]["base_case_name"],
+    )
+    with open(out_dir + "/model_setup.txt", "w") as ms:
+        ms.write(
+            "# Model Name    , Location of Files                                                                    ,  Shift From,  Shift To\n",  # noqa: E501
+        )
+        ms.write(
+            f"CTSM51          , {base_case_output_dir}/lnd/hist/",
+        )  # TODO: aslo update model name? Add to cupid config file?
+    print(f"wrote {out_dir}/model_setup.txt")
+    print("You can now run ILAMB with the following commands:")
+    print("---")
+    print("qinteractive -l select=1:ncpus=16:mpiprocs=16:mem=100G -l walltime=06:00:00")
+    print("conda activate cupid-analysis")
+    print("export ILAMB_ROOT=../../ilamb_aux")
     if run_type == "BGC":
-        ilamb_config = "ilamb_nohoff_final_CLM_template.cfg"
-    elif run_type == "SP":
-        ilamb_config = "ilamb_nohoff_final_CLM_SP_template.cfg"
-    with open(ilamb_config, encoding="UTF-8") as i_config:
-        i_dict = yaml.safe_load(i_config)
-        # TODO: PROBABLY ACTUALLY JUST NEED TO EDIT GLADE/CAMPAIGN PATHS
-
-    # read parameters from CUPID
-    # use `get` to default to None
-    # DOUT = c_dict["global_params"]["CESM_output_dir"]
-    # base_case_name = c_dict["global_params"]["base_case_name"]
-    # test_case_name = c_dict["global_params"]["case_name"]
-
-    with open(out_dir + "/" + ilamb_config, "w") as f:
-        # Header of file is a comment logging provenance
-        f.write(
-            "# This file has been auto-generated using generate_ilamb_config_file.py\n",
+        print(
+            "mpiexec ilamb-run --config ilamb_nohoff_final_CLM.cfg --build_dir bld/ --df_errs ../../ilamb_aux/quantiles_Whittaker_cmip5v6.parquet --define_regions ../../ilamb_aux/DATA/regions/LandRegions.nc ../../ilamb_aux/DATA/regions/Whittaker.nc --regions global --model_setup model_setup.txt --filter .clm2.h0.",  # noqa: E501
         )
-        f.write(f"# It is based off of {cupid_config_loc}/config.yml\n")
-        f.write("# Arguments:\n")
-        f.write(f"# {cesm_root=}\n")
-        f.write(f"# {cupid_config_loc=}\n")
-        f.write(f"# {run_type=}\n")
-        # enter in each element of the dictionary into the new file
-        yaml.dump(i_dict, f, sort_keys=False)
-        # TODO: not a yaml file...
-
-
-def generate_ilamb_model_setup(cesm_root, cupid_config_loc, run_type, out_dir):
-    """Create model_setup.txt file for use in ILAMB"""
-    sys.path.append(os.path.join(cesm_root, "cime"))
-
-    # cupid_root = os.path.join(cesm_root, "tools", "CUPiD")
-    # # Is cupid_config_loc a valid value?
-    # if cupid_config_loc is None:
-    #     cupid_config_loc = os.path.join(cupid_root, "examples", "key_metrics")
-    # if not os.path.exists(os.path.join(cupid_config_loc, "config.yml")):
-    #     raise KeyError(f"Can not find config.yml in {cupid_config_loc}")
-
-    # with open(os.path.join(cupid_config_loc, "config.yml")) as c:
-    #     c_dict = yaml.safe_load(c)
-    with open("model_setup.txt", "w") as ms:
-
-        ms.write(
-            "# Model Name    , Location of Files                                                                                     ,  Shift From,  Shift To\n",  # noqa: E501
+    if run_type == "SP":
+        print(
+            "mpiexec ilamb-run --config ilamb_nohoff_final_CLM_SP.cfg --build_dir bld/ --df_errs ../../ilamb_aux/quantiles_Whittaker_cmip5v6.parquet --define_regions ../../ilamb_aux/DATA/regions/LandRegions.nc ../../ilamb_aux/DATA/regions/Whittaker.nc --regions global --model_setup model_setup.txt --filter .clm2.h0.",  # noqa: E501
         )
-        ms.write(
-            "CTSM51          , /glade/campaign/cgd/tss/common/Land_Only_Simulations/CTSM52_DEV/ctsm51_ctsm51d166deadveg_1deg_CRUJRA_FLDS_ABsnoCDE_blk_A5BCD_hist/lnd/hist/",  # noqa: E501
-        )
+    print("---")
 
 
 if __name__ == "__main__":
     args = vars(_parse_args())
     print(args)
-    generate_ilamb_config(
-        args["cesm_root"],
-        args["cupid_config_loc"],
-        args["run_type"],
-        args["out_dir"],
-    )
     generate_ilamb_model_setup(
         args["cesm_root"],
         args["cupid_config_loc"],
