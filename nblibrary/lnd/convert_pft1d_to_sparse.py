766c8688e9dcbc662b1af38de84a2c271f88550d
Sam Rabin <sam.rabin@gmail.com>
2025-02-20 12:27:49 -0700

    Add vegtype_var, vars_to_grid, and drop_others options to convert_pft1d_to_sparse().

--- nblibrary/lnd/convert_pft1d_to_sparse.py
+++ nblibrary/lnd/convert_pft1d_to_sparse.py
@@ -1,4 +1,4 @@
-# From https://github.com/NCAR/ctsm_python_gallery/blob/02ae0f079bb53b8619408f32b1fb9248ed0a260c/notebooks/sparse-PFT-gridding.ipynb  # noqa:  disable=E501
+# From https://github.com/NCAR/ctsm_python_gallery/blob/02ae0f079bb53b8619408f32b1fb9248ed0a260c/notebooks/sparse-PFT-gridding.ipynb  # noqa: E501
 from __future__ import annotations
 
 import itertools
@@ -31,43 +31,73 @@ def to_sparse(data, vegtype, jxy, ixy, shape):
     )
 
 
-def convert_pft1d_to_sparse(dataset):
+def convert_pft1d_to_sparse(
+    dataset,
+    vegtype_var="pft",
+    vars_to_grid=None,
+    drop_others=False,
+):
 
     # extract PFT variables
-    pfts = xr.Dataset({k: v for k, v in dataset.items() if "pft" in v.dims})
+    pfts = xr.Dataset({k: v for k, v in dataset.items() if vegtype_var in v.dims})
+
+    # Get variables to grid, if not provided
+    if vars_to_grid is None:
+        vars_to_grid = pfts.data_vars.keys()
+    elif not isinstance(vars_to_grid, list):
+        vars_to_grid = [vars_to_grid]
+    for var in vars_to_grid:
+        if var not in dataset:
+            raise KeyError(f"{var} not in dataset")
+    print(f"vars_to_grid: {', '.join(vars_to_grid)}")
 
     ixy = dataset.pfts1d_ixy.astype(int)
     jxy = dataset.pfts1d_jxy.astype(int)
     vegtype = dataset.pfts1d_itype_veg.astype(int)
-    npft = vegtype.max().load().item()
+    nvegtypes = vegtype.max().load().item()
     # expected shape of sparse arrays (excludes time)
     output_sizes = {
-        "pft": npft + 1,
+        vegtype_var: nvegtypes + 1,
         "lat": dataset.sizes["lat"],
         "lon": dataset.sizes["lon"],
     }
 
     result = xr.Dataset()
-    for var in pfts:
-        result[var] = xr.apply_ufunc(
-            to_sparse,
-            pfts[var],
-            vegtype,
-            jxy,
-            ixy,
-            input_core_dims=[["pft"]] * 4,
-            output_core_dims=[["pft", "lat", "lon"]],
-            exclude_dims={"pft"},  # changes size
-            dask="parallelized",
-            kwargs=dict(shape=tuple(output_sizes.values())),
-            dask_gufunc_kwargs=dict(
-                meta=sparse.COO([]),
-                output_sizes=output_sizes,
-            ),  # lets dask know that we are changing from numpy to sparse
-            output_dtypes=[pfts[var].dtype],
-        )
+    for var in dataset:
+        if var in vars_to_grid:
+            if vegtype_var in dataset[var].dims:
+                print(f"Gridding {var}")
+                print(pfts[var])
+                result[var] = xr.apply_ufunc(
+                    to_sparse,
+                    pfts[var],
+                    vegtype,
+                    jxy,
+                    ixy,
+                    input_core_dims=[[vegtype_var]] * 4,
+                    output_core_dims=[[vegtype_var, "lat", "lon"]],
+                    exclude_dims={vegtype_var},  # changes size
+                    dask="parallelized",
+                    kwargs=dict(shape=tuple(output_sizes.values())),
+                    dask_gufunc_kwargs=dict(
+                        meta=sparse.COO([]),
+                        output_sizes=output_sizes,
+                    ),  # lets dask know that we are changing from numpy to sparse
+                )
+            elif all([x in dataset[var].dims for x in ["lat", "lon"]]):
+                result[var] = dataset[var]
+            else:
+                raise RuntimeError(
+                    f"{var} is in vars_to_grid, but it's not a PFT variable "
+                    f"and isn't already gridded. Dims: {dataset[var].dims}",
+                )
+        elif not drop_others:
+            result[var] = dataset[var]
 
     # copy over coordinate variables lat, lon
+    print(result)
     result = result.update(dataset[["lat", "lon"]])
-    result["pft"] = np.arange(result.sizes["pft"])
+    print(result)
+    result["pft"] = np.arange(result.sizes[vegtype_var])
+    print(result)
     return result
