ecec5668eb03f6ec9fa8c433b486e2ec086a1fb3
wwieder <wwieder@ucar.edu>
2025-10-29 16:03:50 -0600

    updating LDF and config files

--- helper_scripts/generate_ldf_config_file.py
+++ helper_scripts/generate_ldf_config_file.py
@@ -110,14 +110,16 @@ def generate_ldf_config(cupid_config_loc, ldf_template, out_file):
     )
     start_date = get_date_from_ts(c_ts["lnd"], "start_years", test_case_cupid_ts_index)
     end_date = get_date_from_ts(c_ts["lnd"], "end_years", test_case_cupid_ts_index)
-    a_dict["diag_cam_climo"]["start_year"] = c_dict["global_params"].get(
-        "climo_start_date",
+    a_dict["diag_cam_climo"]["climo_start_year"] = c_dict["global_params"].get(
+        "climo_start_year",
         start_date,
     )
-    a_dict["diag_cam_climo"]["end_year"] = c_dict["global_params"].get(
-        "climo_end_date",
+    a_dict["diag_cam_climo"]["start_year"] = start_date
+    a_dict["diag_cam_climo"]["climo_end_year"] = c_dict["global_params"].get(
+        "climo_end_year",
         end_date,
     )
+    a_dict["diag_cam_climo"]["end_year"] = end_date
 
     # Set values for BASELINE
     base_case_cupid_ts_index = (
@@ -182,17 +184,24 @@ def generate_ldf_config(cupid_config_loc, ldf_template, out_file):
             )
     except:  # noqa: E722
         pass
-    a_dict["diag_cam_baseline_climo"]["start_year"] = c_dict["global_params"].get(
-        "base_climo_start_date",
+
+    a_dict["diag_cam_baseline_climo"]["climo_start_year"] = c_dict["global_params"].get(
+        "base_climo_start_year",
         base_start_date,
     )
-    a_dict["diag_cam_baseline_climo"]["end_year"] = c_dict["global_params"].get(
-        "base_climo_end_date",
+    a_dict["diag_cam_baseline_climo"]["start_year"] = base_start_date
+    a_dict["diag_cam_baseline_climo"]["climo_end_year"] = c_dict["global_params"].get(
+        "base_climo_end_year",
         base_end_date,
     )
+    a_dict["diag_cam_baseline_climo"]["end_year"] = base_end_date
+
     a_dict["diag_basic_info"]["defaults_file"] = c_dict["compute_notebooks"]["lnd"][
         "LDF"
     ]["external_tool"]["defaults_file"]
+    a_dict["diag_basic_info"]["regions_file"] = c_dict["compute_notebooks"]["lnd"][
+        "LDF"
+    ]["external_tool"]["regions_file"]
     a_dict["diag_basic_info"]["hist_str"] = c_dict["timeseries"]["lnd"]["hist_str"]
     a_dict["diag_basic_info"]["num_procs"] = c_dict["timeseries"].get("num_procs", 1)
     a_dict["diag_basic_info"]["cam_regrid_loc"] = os.path.join(
