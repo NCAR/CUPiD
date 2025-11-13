4a7ccb0f660ba04ef8e48078c14b7f4846f38ac1
Teagan King <tking@ucar.edu>
2025-11-13 11:22:40 -0700

    include logic for changing index to summary tables if ADF/LDF/ILAMB were run test

--- helper_scripts/generate_cupid_config_for_cesm_case.py
+++ helper_scripts/generate_cupid_config_for_cesm_case.py
@@ -55,6 +55,21 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
     default=None,
     help="Directory where ADF will be run (None => case root)",
 )
+@click.option(
+    "--cupid-run-adf",
+    default=None,
+    help="Boolean flag to indicate whether to run ADF analysis",
+)
+@click.option(
+    "--cupid-run-ldf",
+    default=None,
+    help="Boolean flag to indicate whether to run LDF analysis",
+)
+@click.option(
+    "--cupid-run-ilamb",
+    default=None,
+    help="Boolean flag to indicate whether to run ILAMB analysis",
+)
 def generate_cupid_config(
     case_root,
     cesm_root,
@@ -68,6 +83,9 @@ def generate_cupid_config(
     cupid_base_startdate,
     cupid_base_enddate,
     adf_output_root,
+    cupid_run_adf,
+    cupid_run_ldf,
+    cupid_run_ilamb,
 ):
     """
     Generate a CUPiD `config.yml` file based on information from a CESM case and
@@ -119,6 +137,18 @@ def generate_cupid_config(
 
     cupid_base_enddate : str
         The end date of the base case ("YYYY-MM-DD").
+
+    adf_output_root : str
+        The root directory where ADF output will be stored (defaults to case_root).
+
+    cupid_run_adf : Bool
+        Boolean flag to indicate whether to run ADF analysis.
+
+    cupid_run_ldf : Bool
+        Boolean flag to indicate whether to run LDF analysis.
+
+    cupid_run_ilamb : Bool
+        Boolean flag to indicate whether to run ILAMB analysis.
 
     Raises:
     -------
@@ -215,6 +245,7 @@ def generate_cupid_config(
                 cupid_start_year,
                 cupid_base_start_year,
             ]
+
     if "link_to_ADF" in my_dict["compute_notebooks"].get("atm", {}):
         my_dict["compute_notebooks"]["atm"]["link_to_ADF"]["parameter_groups"]["none"][
             "adf_root"
