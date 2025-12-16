1094d5af6603b60f356949c21216303319659ae9
Teagan King <tking@ucar.edu>
2025-12-16 11:51:19 -0700

    address review, use ts_dir, spatial means streamlined, extraneous config values removed

--- cupid_utils/cupid_utils/multi-component/spatial_means.py
+++ cupid_utils/cupid_utils/multi-component/spatial_means.py
@@ -4,39 +4,6 @@ import xarray as xr
 import numpy as np
 
 
-def fix_time_variable(ds):
-    """
-    Description: fix the time of cam file to have the time at
-       the middle of the interval instead of the end the interval.
-    Argument:
-       ds: dataset
-    Return:
-       ds: dataset with the time variable at the middle of interval.
-    """
-    time = ds["time"]
-    time = xr.DataArray(
-        ds["time_bnds"].load().mean(dim="nbnd").values,
-        dims=time.dims,
-        attrs=time.attrs,
-    )
-    ds["time"] = time
-    ds.assign_coords(time=time)
-    return ds
-
-
-def ann_mean(ds):
-    """
-    Description: compute the annual mean of a monthly mean dataset
-    Argument:
-       ds: dataset "
-    Return:
-       ds_ann: annual mean
-    """
-    ds_gb = ds.groupby("time.year")
-    ds_ann = ds_gb.mean()
-    return ds_ann
-
-
 def global_mean(variable):
     """
     Description: compute the global mean (weigthed) of a variable
@@ -46,9 +13,35 @@ def global_mean(variable):
        ds_ann: annual mean
     """
     weights = np.cos(np.deg2rad(variable.lat))
-    variable_weigthed = variable.weighted(weights)
-    variable_mean = variable_weigthed.mean(("lon", "lat"))
+    variable_weighted = variable.weighted(weights)
+    variable_mean = variable_weighted.mean(("lon", "lat"))
     return variable_mean
+
+
+def compute_ann_mean(filepath, case, var, lat1=None, lat2=None, lon1=None, lon2=None):
+    """
+    Description: Compute the annual mean within a specified latitude (lat1-lat2)
+    and longitude (lon1-lon2) range of the variable "var".
+
+    Arguments:
+        filepath = filepath
+        case = casename
+        var = variable name
+        lat1 = starting latitude
+        lat2 = ending latitude
+        lon1 = starting longitude
+        lon2 = ending longitude
+    """
+    filename = filepath + case + "/" + case + "." + var + ".nc"
+    ds = xr.open_dataset(filename)
+
+    # Select the subset of data within the specified latitude and longitude ranges before calculating the annual mean
+    ds_subset = ds.sel(lat=slice(lat1, lat2), lon=slice(lon1, lon2))
+
+    var_ann = ds_subset[var].groupby("time.year").mean()
+    var_lat_lon_ann = lat_lon_mean(var_ann, lat1, lat2, lon1, lon2)
+
+    return var_lat_lon_ann
 
 
 def compute_var_g_ann(filepath, case, var):
@@ -61,11 +54,7 @@ def compute_var_g_ann(filepath, case, var):
     """
     filename = filepath + case + "/" + case + "." + var + ".nc"
     ds = xr.open_dataset(filename)
-
-    ds.mean(["lat", "time"])
-    var_ann = ann_mean(ds[var])
-    var_g_ann = global_mean(var_ann)
-    return var_g_ann
+    return global_mean(ds[var].groupby("time.year").mean())
 
 
 def lat_lon_mean(variable, lat1, lat2, lon1, lon2):
@@ -122,35 +111,10 @@ def lat_lon_mean_norm(variable, lat1, lat2, lon1, lon2):
     weights /= weights.sum(dim="lat")
 
     # Apply weighted mean over the subset
-    variable_weigthed = variable_subset.weighted(weights)
-    variable_mean = variable_weigthed.mean(("lon", "lat"))
+    variable_weighted = variable_subset.weighted(weights)
+    variable_mean = variable_weighted.mean(("lon", "lat"))
 
     return variable_mean
-
-
-def compute_var_lat_lon_ann(filepath, case, var, lat1, lat2, lon1, lon2):
-    """
-    Description: Compute the annual mean within a specified latitude (lat1-lat2)
-    and longitude (lon1-lon2) range of the variable "var".
-
-    Arguments:
-       case = casename
-       var = variable name
-       lat1 = starting latitude
-       lat2 = ending latitude
-       lon1 = starting longitude
-       lon2 = ending longitude
-    """
-    filename = filepath + case + "/" + case + "." + var + ".nc"
-    ds = xr.open_dataset(filename)
-
-    # Select the subset of data within the specified latitude and longitude ranges before calculating the annual mean
-    ds_subset = ds.sel(lat=slice(lat1, lat2), lon=slice(lon1, lon2))
-
-    var_ann = ann_mean(ds_subset[var])
-    var_lat_lon_ann = lat_lon_mean(var_ann, lat1, lat2, lon1, lon2)
-
-    return var_lat_lon_ann
 
 
 def compute_var_zonal_ann(filepath, case, var):
@@ -164,5 +128,5 @@ def compute_var_zonal_ann(filepath, case, var):
     filename = filepath + case + "/" + case + "." + var + ".nc"
     ds = xr.open_dataset(filename)
     ds.mean(["lon"])
-    var_zonal_ann = ann_mean(ds[var]).mean(["lon"])
+    var_zonal_ann = ds[var].groupby("time.year").mean().mean(["lon"])
     return var_zonal_ann
