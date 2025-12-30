a6cc4fc0144b7a84bb6f3cebbbba8e8b40032745
Sam Rabin <sam.rabin@gmail.com>
2025-12-30 10:06:46 -0700

    Interpolate EarthStat if needed to match CLM.

--- nblibrary/lnd/earthstat.py
+++ nblibrary/lnd/earthstat.py
@@ -5,8 +5,17 @@ from __future__ import annotations
 
 import os
 import warnings
+from copy import deepcopy
 
+import numpy as np
 import xarray as xr
+import xesmf as xe
+
+# The resolution which will be used as the basis for regridding, if available
+REFERENCE_EARTHSTAT_RES = "f09"
+
+# One level's worth of indentation for messages
+INDENT = "    "
 
 
 def align_time(da_to_align, target_time):
@@ -56,6 +65,122 @@ def check_dim_alignment(earthstat_ds, clm_ds):
     return earthstat_ds
 
 
+def _get_regridder_and_mask(da_in, mask_in, ds_target, method):
+    """Get regridder and mask"""
+    # Create mask
+    mask = xr.where(~mask_in.notnull() | (mask_in == 0), 0.0, 1.0)
+
+    # Not sure how much of a difference specifying this makes, but anyway
+    if "conservative" in method:
+        # Extrapolation not available for conservative regridding methods
+        extrap_method = None
+    else:
+        extrap_method = "inverse_dist"
+
+    # Create and apply regridder
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="Latitude is outside of")
+        regridder = xe.Regridder(
+            da_in,
+            ds_target,
+            method=method,
+            ignore_degenerate=True,
+            extrap_method=extrap_method,
+        )
+    mask_regridded = regridder(mask)
+
+    return regridder, mask_regridded
+
+
+def _regrid_to_clm(
+    *,
+    ds_in,
+    var,
+    ds_target,
+    method="conservative",
+    area_in=None,
+    area_out=None,
+    mask_var=None,
+):
+    """Regrid an observational dataset to a CLM target grid using conservative regridding"""
+
+    # Sense checks
+    assert (area_in is None) == (area_out is None)
+    if area_in is not None:
+        area_out = area_out * area_in.sum() / area_out.sum()
+
+    # Get mask of input data
+    if mask_var is None:
+        mask_var = var
+    mask_in = ds_in[mask_var]
+
+    # Extract data
+    da_in = ds_in[var]
+    if area_in is not None:
+        # da_in /= area_in
+        da_in = da_in / area_in
+        da_in = da_in.where(area_in > 0)
+    data_filled = da_in.fillna(0.0)
+
+    # xesmf might look for a "mask" variable:
+    # https://coecms-training.github.io/parallel/case-studies/regridding.html
+    if "landmask" in ds_target:
+        ds_target["mask"] = ds_target["landmask"].fillna(0)
+
+    # Create and apply regridder
+    regridder, _ = _get_regridder_and_mask(
+        da_in,
+        mask_in,
+        ds_target,
+        method,
+    )
+    da_out = regridder(data_filled)
+
+    # NO; this adds an 8-9 percentage point overestimate in test variables HarvestArea and
+    # Production. Which makes sense: These are not per-m2-of-gridcell numbers; these are gridcell
+    # TOTALS.
+    # # Normalize to account for partial coverage
+    # da_out = da_out / mask_regridded
+
+    # apply landmask from CLM target grid
+    da_out = da_out * ds_target["landmask"]
+
+    if area_out is not None:
+        da_out *= area_out
+
+    # If we chose a conservative method, assume we want the global sums to match before and after.
+    if "conservative" in method:
+
+        # Print error in global sum introduced by regridding
+        before = ds_in[var].sum().values
+        after = da_out.sum().values
+        pct_diff = (after - before) / before * 100
+        if "units" in ds_in[var].attrs:
+            units = ds_in[var].attrs["units"]
+        else:
+            units = "unknown units"
+        print(f"{INDENT}{var} ({units}):")
+        print(f"{2*INDENT}Global sum before: {before:.2e}")
+        print(f"{2*INDENT}Global sum  after: {after:.2e} ({pct_diff:.1f}% diff)")
+
+        # Adjust so global sum matches what we had before regridding
+        print(f"{2*INDENT}Adjusting to match.")
+        da_out = da_out * ds_in[var].sum() / da_out.sum()
+        after = da_out.sum().values
+        diff = after - before
+        pct_diff = diff / before * 100
+        print(f"{2*INDENT}Global diff final: {diff:.2e} ({pct_diff:.1f}%)")
+
+    # Recover attributes
+    da_out.attrs = ds_in[var].attrs
+
+    return da_out
+
+
+def get_clm_landarea(ds):
+    return ds["area"] * ds["landfrac"]
+
+
 class EarthStat:
     """
     A class to handle importing EarthStat data
@@ -75,7 +200,7 @@ class EarthStat:
         self._get_crop_list(earthstat_dir, opts["crops_to_include"])
 
         # Import EarthStat maps
-        self._import_data(earthstat_dir, sim_resolutions, opts)
+        self._import_all_resolutions(earthstat_dir, sim_resolutions, opts)
 
     def __getitem__(self, key):
         """instance[key] syntax should return corresponding value in data dict"""
@@ -95,6 +220,12 @@ class EarthStat:
         Return keys of the _data dict
         """
         return self._data.keys()
+
+    def items(self):
+        """
+        Return items (key-value pairs) of the _data dict
+        """
+        return self._data.items()
 
     def _get_crop_list(self, earthstat_dir, crops_to_include):
         """
@@ -131,29 +262,105 @@ class EarthStat:
             if crop not in self.crops:
                 print(f"WARNING: {crop} not found in self.crops")
 
-    def _import_data(self, earthstat_dir, sim_resolutions, opts):
+    def _import_all_resolutions(self, earthstat_dir, sim_resolutions, opts):
         """
         Import EarthStat maps corresponding to simulated CLM resolutions
         """
-        for res in sim_resolutions:
-            # Skip (but warn) if EarthStat file not found.
-            earthstat_file = os.path.join(earthstat_dir, res + ".nc")
-            print(earthstat_file)
-            if not os.path.exists(earthstat_file):
-                warnings.warn(
-                    f"No EarthStat maps available for resolution {res}; this will cause problems",
+        for res in sim_resolutions.keys():
+            self._import_one_resolution(earthstat_dir, opts, res)
+
+        # If any resolutions weren't read, we'll need to interpolate to get them
+        missing_res = [k for k in sim_resolutions.keys() if k not in self.keys()]
+        if missing_res:
+            # Get base resolution to do regridding from
+            if REFERENCE_EARTHSTAT_RES not in self.keys():
+                self._import_one_resolution(
+                    earthstat_dir,
+                    opts,
+                    REFERENCE_EARTHSTAT_RES,
                 )
-                continue
+            # If no resolutions were read, we have a problem
+            if not self.keys():
+                raise FileNotFoundError("No EarthStat files read")
+            # If REFERENCE_EARTHSTAT_RES not available, fall back to the finest resolution we have.
+            if REFERENCE_EARTHSTAT_RES in self.keys():
+                earthstat_in_ds = self[REFERENCE_EARTHSTAT_RES]
+                earthstat_in_res = REFERENCE_EARTHSTAT_RES
+            else:
+                n_finest = -np.inf
+                for res, ds in self.items():
+                    n = ds["lat"].size
+                    if n > n_finest:
+                        earthstat_in_ds = ds
+                        earthstat_in_res = res
+                        n_finest = n
 
-            # Open file
-            print(f"Importing EarthStat yield maps for resolution {res}...")
-            ds = xr.open_dataset(earthstat_file)
-            start_year = opts["start_year"]
-            end_year = opts["end_year"]
-            ds = ds.sel(time=slice(f"{start_year}-01-01", f"{end_year}-12-31"))
+            if earthstat_in_res in sim_resolutions:
+                clm_in_ds = sim_resolutions[earthstat_in_res]
+            else:
+                clm_in_file = os.path.join(
+                    earthstat_dir,
+                    f"{earthstat_in_res}_clm_regrid_ds.nc",
+                )
+                clm_in_ds = xr.open_dataset(clm_in_file)
+            clm_in_landarea = get_clm_landarea(clm_in_ds)
 
-            self[res] = ds
+            for res in missing_res:
+                print(f"Interpolating EarthStat data from {earthstat_in_res} to {res}")
+                clm_out_ds = sim_resolutions[res]
+                clm_out_landarea = get_clm_landarea(clm_out_ds)
+
+                # Interpolate
+                for i, var in enumerate(["HarvestArea", "Production"]):
+                    match var:
+                        case "HarvestArea" | "Production":
+                            method = "conservative"
+                        case _:
+                            raise ValueError(f"Undefined interp method for var {var}")
+                    with warnings.catch_warnings():
+                        warnings.filterwarnings("ignore", message=".*large graph.*")
+                        earthstat_out_da = _regrid_to_clm(
+                            ds_in=deepcopy(earthstat_in_ds),
+                            var=var,
+                            ds_target=deepcopy(clm_out_ds),
+                            method=method,
+                            area_in=deepcopy(clm_in_landarea),
+                            area_out=deepcopy(clm_out_landarea),
+                            mask_var="LandMask",
+                        )
+                    if i == 0:
+                        self[res] = xr.Dataset(data_vars={var: earthstat_out_da})
+                    else:
+                        self[res][var] = earthstat_out_da
+
+                # Calculate yield
+                self[res]["Yield"] = self[res]["Production"] / self[res]["HarvestArea"]
+                with warnings.catch_warnings():
+                    warnings.filterwarnings("ignore", message=".*large graph.*")
+                    self[res]["Yield"].load()
+                self[res]["Yield"].attrs["long_name"] = "crop yield"
+                prod_units = self[res]["Production"].attrs["units"]
+                area_units = self[res]["HarvestArea"].attrs["units"]
+                self[res]["Yield"].attrs["units"] = f"{prod_units}/{area_units}"
+
         print("Done.")
+
+    def _import_one_resolution(self, earthstat_dir, opts, res):
+        earthstat_file = os.path.join(earthstat_dir, res + ".nc")
+        # Skip (but warn) if EarthStat file not found.
+        if not os.path.exists(earthstat_file):
+            print(f"No EarthStat maps available for resolution {res}; will interpolate")
+            return
+
+        # Open file
+        print(f"Importing EarthStat yield maps for resolution {res}...")
+        print(earthstat_file)
+        ds = xr.open_dataset(earthstat_file)
+        start_year = opts["start_year"]
+        end_year = opts["end_year"]
+        ds = ds.sel(time=slice(f"{start_year}-01-01", f"{end_year}-12-31"))
+
+        self[res] = ds
 
     def get_data(self, res, stat_input, crop, target_unit):
         """
