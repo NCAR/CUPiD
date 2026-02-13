a249dc2caaf058a46827adb471d17859e593da05
Michael Levy <mike.levy.work@gmail.com>
2026-02-13 16:03:44 -0700

    Use genTS v0.9.3 and add flexibiltiy
    Writing output for multiple CAM streams, including frequency in the output path
    
    Also updated scripts to process a 15 year run from Nan

--- cupid/run_GenTS.py
+++ cupid/run_GenTS.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 """
 Main script for running GenTS to generate timeseries for all components
+
+For testing:
+
+qinteractive -l select=1:ncpus=12:mem=120GB
 """
 
 import glob
@@ -18,13 +22,14 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 def run_GenTS(
-    dout_s_root="/glade/derecho/scratch/mlevy/GenTS_test/input",
-    ts_outdir="/glade/derecho/scratch/mlevy/GenTS_test/output",
+    # dout_s_root="/glade/derecho/scratch/mlevy/GenTS_test/input/my_test",
+    dout_s_root="/glade/derecho/scratch/nanr/archive/b.e30_alpha08b.B1850C_LTso.ne30_t232_wgx3.cmip7-testing.001",
+    ts_outdir="/glade/derecho/scratch/mlevy/GenTS_test/output/nan_test",
 ):
     config = {}
     config["atmos"] = {
-        "include_patterns": ["*cam.h0a*"],
-        "frequency": "mon",
+        "include_patterns": ["*cam.h0a*", "*cam.h1a*", "*cam.h2a*", "*cam.h3a*"],
+        "frequency": ["mon", "1day", "6hour", "5day"],
         "subdir": "atm",
     }
     config["land"] = {
@@ -32,6 +37,9 @@ def run_GenTS(
         "frequency": "mon",
         "subdir": "lnd",
     }
+
+    # TODO: set up config["seaice"] (or whatever appropriate longname is)
+
     # TODO: include ocn_grid_file and ocn_static_file
     # config["ocean"] = {"include_patterns": ["*mom6.h.native.*"],
     #                    "frequency": "mon",
@@ -48,12 +56,12 @@ def run_GenTS(
         for include_pattern in setup["include_patterns"]:
             cnt = cnt + len(glob.glob(os.path.join(indir, include_pattern)))
         hf_collection = HFCollection(indir, dask_client=client)
-        for include_pattern in setup["include_patterns"]:
+        for frequency, include_pattern in zip(setup["frequency"], setup["include_patterns"]):
             print(f"Processing files with pattern: {include_pattern}")
             hfp_collection = hf_collection.include_patterns([include_pattern])
             hfp_collection.pull_metadata()
             ts_collection = TSCollection(
-                hfp_collection, outdir, ts_orders=None, dask_client=client,
+                hfp_collection, os.path.join(outdir, frequency), ts_orders=None, dask_client=client,
             )
             ts_collection.execute()
 
